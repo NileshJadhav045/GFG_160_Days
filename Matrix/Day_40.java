af4411f8b31b3292adcc70255954b3dd288b67cf
Nilesh Jadhav <nileshjadhav01776@gmail.com>
2025-09-22 22:10:24 +0530

    Day-40

---
+++ Matrix/Day_40.java
@@ -0,0 +1,34 @@
+
+public class Day_40 {
+
+    public static boolean searchMatrix(int[][] mat, int x) {
+
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int lo = 0, hi = n * m - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            int row = mid / m;
+            int col = mid % m;
+
+            if (mat[row][col] == x) {
+                return true;
+            } else if (mat[row][col] < x) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int mat[][] = {{1, 5, 9}, {14, 20, 21}, {30, 34, 43}};
+        int x = 14;
+        System.out.println(searchMatrix(mat, x));
+    }
+}
