96063fe8f65ac239bd40c151d85accfbbd2536e0
Nilesh Jadhav <nileshjadhav01776@gmail.com>
2025-10-03 16:24:25 +0530

    Day-51

---
+++ Two_Pointer/Day_50.java
@@ -0,0 +1,34 @@
+
+public class Day_50 {
+
+    public static int countTriplets(int[] arr, int target) {
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum > target) {
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else if (sum == target) {
+                    count++;
+                    int temp = j + 1;
+                    while (temp < k && arr[temp] == arr[temp - 1]) {
+                        count++;
+                        temp++;
+                    }
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {-3, -1, -1, 0, 1, 2};
+        int target = -2;
+        System.out.println(countTriplets(arr, target));
+    }
+}
