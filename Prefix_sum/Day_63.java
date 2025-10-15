48bb321072e684d2c50cda3467eb77e8b2859be6
Nilesh Jadhav <nileshjadhav01776@gmail.com>
2025-10-15 20:23:51 +0530

    Day-63

---
+++ Prefix_sum/Day_63.java
@@ -0,0 +1,33 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day_63 {
+
+    public static int maxLen(int[] arr) {
+        // Your code here
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += (arr[i] == 0) ? -1 : 1;
+
+            if (sum == 0) {
+                maxLen = i + 1;
+            }
+            if (map.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 0, 1, 1, 1, 0, 0};
+        System.out.println(maxLen(arr));
+    }
+}
